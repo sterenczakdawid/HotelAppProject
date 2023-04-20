1ac9f4cfd87257370076fdd0177da0a8c3e85d26
Dawid <sterenczak.dawid@gmail.com>
2023-04-20 20:53:26 +0200

    working on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
 	<div className = "navbar">
-		<Link to="/">
-			<img src="" alt="" />
-			<span>Hotel</span>
+		<Link className = "logo" to="/">
+			{/* <img src="" alt="" /> */}
+			<span>BigDickHotel</span>
 		</Link>
-		<nav>
-			<Link className = "nav-item" to = "/">Home</Link>
-			<Link className = "nav-item" to = "/contact">Contact</Link>
+		<nav className = "nav">
+			<NavLink className = "nav-item" to = "/"><i class="fa-solid fa-house-user"/>Home</NavLink>
+			<NavLink className = "nav-item" to = "/contact">Contact</NavLink>
 			<button className = "btn">Login</button>
 		</nav>
 	</div>
